3dcd6ca7b5cdf6b6e8a896a022b039f267a9b141
arthurariza <tucaariza@gmail.com>
2022-05-05 01:04:14 -0300

    adding helpers and custom helpers

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,5 @@ Movie.create(title: 'Superman', rating: 'PG', total_gross: 300_451_603,
              description: 'Clark Kent grows up to be the greatest super-hero', released_on: '1978-12-15')
 Movie.create(title: 'Spider-Man', rating: 'PG-13', total_gross: 825_025_036,
              description: 'Peter Parker gets bitten by a genetically modified spider', released_on: '2002-05-03')
+Movie.create(title: 'Fantastic Four', rating: 'PG-13', total_gross: 168_257_860,
+             description: 'Four young outsiders teleport to an alternate and dangerous universe', released_on: '2015-08-07')
