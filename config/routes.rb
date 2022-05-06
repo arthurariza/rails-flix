810e2611cd2ef26e039d7e32a99661eff705aa11
arthurariza <tucaariza@gmail.com>
2022-05-05 22:41:01 -0300

    Linking between pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root 'movies#index'
   get 'movies' => 'movies#index'
-  get 'movies/:id' => 'movies#show'
+  get 'movies/:id' => 'movies#show', as: :movie
 end
