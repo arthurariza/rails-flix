7e9214999a32b5296e7177d6a2af43bbfd438f86
arthurariza <tucaariza@gmail.com>
2022-05-07 15:37:34 -0300

    refactor routes to resource refactor blank lines in erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root 'movies#index'
-  get 'movies' => 'movies#index'
-  get 'movies/:id' => 'movies#show', as: :movie
-  patch 'movies/:id' => 'movies#update'
-  get 'movies/:id/edit' => 'movies#edit', as: :edit_movie
+  resources :movies
 end
