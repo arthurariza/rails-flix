d8377dbff58904b9b1d40eb8d8061defdc3634ad
arthurariza <tucaariza@gmail.com>
2022-05-07 15:05:51 -0300

    Edit movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,29 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: %w[show edit update]
+
   def index
     @movies = Movie.all
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_movie
     @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'movies#index'
   get 'movies' => 'movies#index'
   get 'movies/:id' => 'movies#show', as: :movie
+  patch 'movies/:id' => 'movies#update'
+  get 'movies/:id/edit' => 'movies#edit', as: :edit_movie
 end
