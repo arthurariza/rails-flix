d0adacf3e25dadbaa46b9b630df84f443f1b1d2b
arthurariza <tucaariza@gmail.com>
2022-05-08 18:42:22 -0300

    Move has many Reviews Reviews cascade delete Reviews validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :movie
+
+  STARTS = [1, 2, 3, 4, 5].freeze
+
+  validates :name, presence: true
+  validates :comment, length: { minimum: 4 }
+  validates :starts, inclusion: {
+    in: STARS,
+    message: 'must be between 1 and 5'
+  }
 end
