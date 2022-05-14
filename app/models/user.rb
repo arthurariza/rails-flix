7de104496c6f81944473fa72c0b48735e047d096
arthurariza <tucaariza@gmail.com>
2022-05-14 15:48:06 -0300

    User signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presente: true
+  validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 10, allow_blank: true }
+  validates :password, length: { minimum: 6, allow_blank: true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  resources :users
   root 'movies#index'
   resources :movies do
     resources :reviews
   end
+  resources :users
+  get 'signup', to: 'users#new'
 end
