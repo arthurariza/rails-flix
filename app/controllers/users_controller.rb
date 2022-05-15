7de383b9b24d6b6441b19d06a1312357af98e06a
arthurariza <tucaariza@gmail.com>
2022-05-15 15:10:12 -0300

    feat: Authorization
    *Added require_signin method to Application Controller to require login
    
    *Moved current user to Application Controller
    
    *Created set_user to reduce some code duplication on User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_signin, except: %i[new create]
+
   def index
     @users = User.all
   end
 
-  def show
-    @user = User.find_by_id(params[:id])
-  end
+  def show; end
 
   def new
     @user = User.new
@@ -22,12 +23,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: 'Account successfully updated!'
     else
@@ -36,13 +34,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to movies_url, alert: 'Account successfully deleted!'
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
