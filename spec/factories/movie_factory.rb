1090b3a935349afd806cfa9e5db300cd7826c123
arthurariza <tucaariza@gmail.com>
2022-06-08 00:38:16 -0300

    feat: Added faker gem

--- spec/factories/movie_factory.rb
+++ spec/factories/movie_factory.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
+require 'faker'
+
 FactoryBot.define do
   factory :movie do
-    title { 'Avengers: Endgame' }
-    description do
-      %(
-      After the devastating events of Avengers: Infinity War, the universe
-      is in ruins. With the help of remaining allies, the Avengers assemble
-      once more in order to undo Thanos' actions and restore order to the universe.
-    ).squish
-    end
+    title { Faker::Movie.unique.title }
+    description { Faker::Lorem.paragraph }
     released_on { '2019-04-26' }
     rating { 'PG-13' }
     total_gross { 1_223_641_414 }
-    director { 'Anthony Russo' }
+    director { Faker::Name.name }
     duration { '181 min' }
     image_file_name { 'avengers-end-game.png' }
   end
--- spec/factories/review_factory.rb
+++ spec/factories/review_factory.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+require 'faker'
+
 FactoryBot.define do
   factory :review do
-    comment { 'Mock comment for testing' }
+    comment { Faker::Lorem.paragraph }
   end
 end
